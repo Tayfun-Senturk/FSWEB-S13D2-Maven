9b4b668802498e355e72c30230e084f5e089109f
Tayfun-senturk <tayfunsenturk8@gmail.com>
2024-09-18 01:55:49 +0300

    Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,26 +5,68 @@ public class Main {
         System.out.println("Hello world!");
 
     }
-    public static boolean isPalindrome(int x){
+
+    public static boolean isPalindrome(int x) {
         int mutlak = Math.abs(x);
-        String sayi= Integer.toString(mutlak);
-        String ters= "";
-        for (int i=sayi.length()-1;i>=0;i--){
-            ters+=sayi.charAt(i);
+        String sayi = Integer.toString(mutlak);
+        String ters = "";
+        for (int i = sayi.length() - 1; i >= 0; i--) {
+            ters += sayi.charAt(i);
         }
         return ters.equals(sayi);
     }
-    public static boolean isPerfectNumber(int x){
-        int sonuc=0;
-        for (int i=1;i<=x/2;i++){
-            if (x%i==0){
+
+    public static boolean isPerfectNumber(int x) {
+        int sonuc = 0;
+        for (int i = 1; i <= x / 2; i++) {
+            if (x % i == 0) {
                 sonuc += i;
             }
 
         }
-        return sonuc==x;
+        return sonuc == x;
     }
-    public static String numberToWords(int x){
 
+    public static String numberToWords(int x) {
+        if (x<0){
+            return "Invalid Value";
+        }
+        String sayi = Integer.toString(x);
+        StringBuilder sonuc = new StringBuilder();
+        for (int i = 0; i < sayi.length(); i++) {
+            switch (Character.getNumericValue(sayi.charAt(i))) {
+                case 0:
+                    sonuc.append("Zero ");
+                    break;
+                case 1:
+                    sonuc.append("One ");
+                    break;
+                case 2:
+                    sonuc.append("Two ");
+                    break;
+                case 3:
+                    sonuc.append("Three ");
+                    break;
+                case 4:
+                    sonuc.append("Four ");
+                    break;
+                case 5:
+                    sonuc.append("Five ");
+                    break;
+                case 6:
+                    sonuc.append("Six ");
+                    break;
+                case 7:
+                    sonuc.append("Seven ");
+                    break;
+                case 8:
+                    sonuc.append("Eight ");
+                    break;
+                case 9:
+                    sonuc.append("Nine ");
+                    break;
+            }
+        }
+        return sonuc.toString().trim();
     }
 }
