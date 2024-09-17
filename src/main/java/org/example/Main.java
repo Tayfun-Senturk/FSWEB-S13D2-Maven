aaa59b265253368c4ad6c39372944f0ac161a9f0
Tayfun-senturk <tayfunsenturk8@gmail.com>
2024-09-18 01:35:04 +0300

    last one

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,28 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    }
+    public static boolean isPalindrome(int x){
+        int mutlak = Math.abs(x);
+        String sayi= Integer.toString(mutlak);
+        String ters= "";
+        for (int i=sayi.length()-1;i>=0;i--){
+            ters+=sayi.charAt(i);
+        }
+        return ters.equals(sayi);
+    }
+    public static boolean isPerfectNumber(int x){
+        int sonuc=0;
+        for (int i=1;i<=x/2;i++){
+            if (x%i==0){
+                sonuc += i;
+            }
+
+        }
+        return sonuc==x;
+    }
+    public static String numberToWords(int x){
+
     }
 }
